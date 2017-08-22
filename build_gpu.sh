b3f225586b1456c2153b47d4133bb0cbf1661507
fernandocamargo <fernando.camargo.ti@gmail.com>
2017-08-22 13:53:52 -0300

    Refactored the Docker configuration to allow a CPU and a GPU version of the backend.

---
+++ build_gpu.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export BASE_IMAGE=nvidia/cuda:8.0-cudnn7-devel
+
+docker-compose build
--- run_gpu.sh
+++ run_gpu.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # prod
+export BASE_IMAGE=nvidia/cuda:8.0-cudnn7-devel
 export ENV=prod
 
 docker-compose up -d
